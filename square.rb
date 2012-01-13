1f4853618fc2331713d8597492a895738e81b0e6
Ronald Hecker <selsery@gmail.com>
2012-01-13 21:21:38 +0100

    Added triangle and square lesson

---
+++ square.rb
@@ -0,0 +1,15 @@
+require 'rubygems'
+require "gl"
+require "glut"
+require "mathn"
+
+
+Glut.glutInit
+Glut.glutInitDisplayMode(Glut::GLUT_SINGLE | Glut::GLUT_RGB | Glut::GLUT_DEPTH)
+Glut.glutInitWindowSize(500, 200)
+Glut.glutCreateWindow("Mooi opengl vierkantje")
+#myinit()
+#Glut.glutReshapeFunc(myReshape)
+#Glut.glutDisplayFunc(display)
+#Glut.glutKeyboardFunc(keyboard)
+#Glut.glutMainLoop()
