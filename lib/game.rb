565ec8bfa9838e1c9716c14bebce04673607b1c6
Ronald Hecker <selsery@gmail.com>
2012-01-22 11:54:12 +0100

    Cleanup

--- lib/game.rb
+++ lib/game.rb
@@ -1,52 +1,18 @@
 require 'set'
 require 'window'
 require 'camera'
-require 'scene'
-require 'player'
-require 'arrow'
 
 Window.draw do
 
-  title "OpenGL in Ruby"
+  title "Space wars"
 
-  width 600
-  height 600
+  width 1440
+  height 900
 
   left 10
   top 10
 
   add_camera Camera.new(10, 10, 0, 3, 0)
-
-  add :player, Player.new
-  add :scene, Scene.new
-
-  on "a" do
-    tell(:player).to(:turn_left).and(:walk)
-  end
-
-  on "d" do
-    tell(:player).to(:turn_right).and(:walk)
-  end
-
-  on "w" do
-    tell(:player).to(:jump)
-  end
-
-  on " " do
-    tell(:player).to(:shoot)
-  end
-
-  on "r" do
-    tell(:player).to(:reload)
-  end
-
-  on "-" do
-    camera.zoom_out
-  end
-
-  on "+" do
-    camera.zoom_in
-  end
 
   on "f" do
     enter_fullscreen
